60235c932ae1e8cc2ea7f33fd53362b633f318fc
Chung, Kuan-Wu <521chung@gmail.com>
2017-12-19 15:52:49 +0800

    add Posts

---
+++ app/src/main/java/google/com/nutc106_1project/Posts.java
@@ -0,0 +1,26 @@
+package google.com.nutc106_1project;
+
+/**
+ * Created by User on 2017/12/19.
+ */
+
+public class Posts {
+    private String uid;
+    private String message;
+
+    public String getUid() {
+        return this.uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
+
+    public void setMessage(String msg) {
+        this.message = msg;
+    }
+}
