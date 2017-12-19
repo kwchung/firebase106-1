0f517ab690df4ee7d613d150736016c0f7faaa9e
Chung, Kuan-Wu <521chung@gmail.com>
2017-12-19 16:00:28 +0800

    get Posts List

--- app/src/main/java/google/com/nutc106_1project/MainActivity.java
+++ app/src/main/java/google/com/nutc106_1project/MainActivity.java
@@ -2,12 +2,70 @@ package google.com.nutc106_1project;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView txt_addPost;
+    private Button btn_submit;
+    private FirebaseDatabase database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        database = FirebaseDatabase.getInstance();
+        final DatabaseReference posts = database.getReference().child("posts");
+        txt_addPost = (TextView) findViewById(R.id.txt_addPost);
+        btn_submit = (Button) findViewById(R.id.btn_submit);
+
+        posts.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                Posts p = dataSnapshot.getValue(Posts.class);
+                Log.d("CKW-Posts", "onChildAdded: " + p.getMessage());
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+                Posts p = dataSnapshot.getValue(Posts.class);
+                Log.d("CKW-Posts", "onChildChanged: " + p.getMessage());
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        btn_submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String key = posts.push().getKey();
+                Posts p = new Posts();
+                p.setUid(key);
+                p.setMessage(txt_addPost.getText().toString());
+                posts.child(key).setValue(p);
+            }
+        });
     }
 }
